3e6f98d243498910000da2f3525668955ca8703e
Noah Kim <kimn@ansto.gov.au>
2022-12-01 21:25:29 +1100

    11. Path parameters

--- 03-get-method/app/main.py
+++ 03-get-method/app/main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get('/hello')
 def index():
     return {'message': 'Hello world!'}
+
+
+@app.get('/blog/all')
+def get_all_blogs():
+    return {'message': 'All blogs provided'}
+
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'message': f'Blog with id {id}'}
