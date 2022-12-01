2be97f3ecde07a77bf6f3ed288595d74c1e55a12
Noah Kim <kimn@ansto.gov.au>
2022-12-01 21:30:01 +1100

    12. Predefined values

--- 03-get-method/app/main.py
+++ 03-get-method/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -11,6 +12,17 @@ def index():
 @app.get('/blog/all')
 def get_all_blogs():
     return {'message': 'All blogs provided'}
+
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return {'message': f'Blog type {type}'}
 
 
 @app.get('/blog/{id}')
