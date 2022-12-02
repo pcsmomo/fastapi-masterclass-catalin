369db9338cd9766b6a3d9aeeed4f98cc3e46b040
Noah Kim <dongwoo_333@hanmail.net>
2022-12-02 19:30:33 +1100

    26. Parameter metadata

--- 06-parameters/app/router/blog_post.py
+++ 06-parameters/app/router/blog_post.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 
 
@@ -22,4 +22,19 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'id': id,
         'data': blog,
         'version': version
+    }
+
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel, id: int,
+                   comment_id: int = Query(None,
+                                           title='Id of the comment',
+                                           description='Some description for comment_id',
+                                           alias='commentId',
+                                           deprecated=True)
+                   ):
+    return {
+        'body': blog,
+        'id': id,
+        'comment_id': comment_id
     }
