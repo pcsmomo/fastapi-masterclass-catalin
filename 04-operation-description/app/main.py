099ae40e539356c583ff7d397576c52975bf99dc
Noah Kim <dongwoo_333@hanmail.net>
2022-12-02 18:45:02 +1100

    17. Summary and description

--- 04-operation-description/app/main.py
+++ 04-operation-description/app/main.py
@@ -17,7 +17,9 @@ def index():
 
 @app.get(
     '/blog/all',
-    tags=['blog']
+    tags=['blog'],
+    summary='Retrieve all blogs',
+    description='This api calls simulates fetching all blogs'
 )
 def get_all_blogs(page: int = 1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
@@ -25,6 +27,14 @@ def get_all_blogs(page: int = 1, page_size: Optional[int] = None):
 
 @app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 
