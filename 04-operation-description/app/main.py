4bfa6dc23479705b5fbd680f5e6940be25e85655
Noah Kim <dongwoo_333@hanmail.net>
2022-12-02 18:35:01 +1100

    16. Tags

--- 04-operation-description/app/main.py
+++ 04-operation-description/app/main.py
@@ -15,12 +15,15 @@ def index():
 #     return {'message': 'All blogs provided'}
 
 
-@app.get('/blog/all')
+@app.get(
+    '/blog/all',
+    tags=['blog']
+)
 def get_all_blogs(page: int = 1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
 
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
@@ -31,12 +34,12 @@ class BlogType(str, Enum):
     howto = 'howto'
 
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags=['blog'])
 def get_blog_type(type: BlogType):
     return {'message': f'Blog type {type}'}
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
 def get_blog(id: int, response: Response):
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
