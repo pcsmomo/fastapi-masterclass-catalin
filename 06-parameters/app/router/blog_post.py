3524f00353375b6926c3ea6f74d0e11628282eda
Noah Kim <dongwoo_333@hanmail.net>
2022-12-03 22:19:40 +1100

    30. Complex subtypes

--- 06-parameters/app/router/blog_post.py
+++ 06-parameters/app/router/blog_post.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Dict
 from fastapi import APIRouter, Query, Body, Path
 from pydantic import BaseModel
 
@@ -9,11 +9,19 @@ router = APIRouter(
 )
 
 
+class Image(BaseModel):
+    url: str
+    alias: str
+
+
 class BlogModel(BaseModel):
     title: str
     content: str
     nb_comments: int
     published: Optional[bool]
+    tags: List[str] = []
+    metadata: Dict[str, str] = {'key1': 'val1'}
+    image: Optional[Image] = None
 
 
 @router.post('/new/{id}')
