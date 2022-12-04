e6de5eec6e5ed9c6b4bd81564b5c72cc5874b512
Noah Kim <dongwoo_333@hanmail.net>
2022-12-04 17:33:50 +1100

    40. Create and read

--- 07-sqlalchemy/app/db/db_user.py
+++ 07-sqlalchemy/app/db/db_user.py
@@ -14,3 +14,11 @@ def create_user(db: Session, request: UserBase):
     db.commit()
     db.refresh(new_user)  # to receive auto-generated id
     return new_user
+
+
+def get_all_user(db: Session):
+    return db.query(DbUser).all()
+
+
+def get_user(db: Session, id: int):
+    return db.query(DbUser).filter(DbUser.id == id).first()
--- 07-sqlalchemy/app/router/user.py
+++ 07-sqlalchemy/app/router/user.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.schemas import UserBase
@@ -10,14 +11,20 @@ router = APIRouter(
     tags=['user']
 )
 
-# Create user
-
 
 @router.post('/', response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
 
-# Read user
+
+@router.get('/', response_model=List[UserDisplay])
+def get_all_user(db: Session = Depends(get_db)):
+    return db_user.get_all_user(db)
+
+
+@router.get('/{id}', response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
 
 # Update user
 
