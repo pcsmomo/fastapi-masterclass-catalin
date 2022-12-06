6b3f6b705b54a93e60361a45ae9c8ef2b46cff1d
Noah Kim <dongwoo_333@hanmail.net>
2022-12-07 08:44:43 +1100

    53. Token generation code

--- 09-authentication/app/auth/oauth2.py
+++ 09-authentication/app/auth/oauth2.py
@@ -1,3 +1,22 @@
 from fastapi.security import OAuth2PasswordBearer
+from typing import Optional
+from datetime import datetime, timedelta
+from jose import jwt
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl='token')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = 'a4fbc3a090deb4ee6da272a666faed145568b95b06fe485a4c007e5641083a55'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- 09-authentication/app/router/article.py
+++ 09-authentication/app/router/article.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.schemas import ArticleBase, ArticleDisplay
 from app.db.database import get_db
 from app.db import db_article
-from app.auth.oauth2 import oauth2_schema
+from app.auth.oauth2 import oauth2_scheme
 
 router = APIRouter(
     prefix='/article',
@@ -20,5 +20,5 @@ def create_article(request: ArticleBase, db: Session = Depends(get_db)):
 
 # Get specific article
 @router.get('/{id}', response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)):
+def get_article(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     return db_article.get_article(db, id)
