602cd85a569914bbffccbf88c077de927aac779e
Noah Kim <dongwoo_333@hanmail.net>
2022-12-09 16:50:22 +1100

    61. Section overview

---
+++ 11-tasks/app/auth/oauth2.py
@@ -0,0 +1,47 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from typing import Optional
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.db import db_user
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = 'a4fbc3a090deb4ee6da272a666faed145568b95b06fe485a4c007e5641083a55'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db_user.get_user_by_username(db, username)
+
+    if user is None:
+        raise credentials_exception
+
+    return user
