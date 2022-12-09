a8c744d98ba34ea9a75a469b6ad9103af68b39e8
Noah Kim <dongwoo_333@hanmail.net>
2022-12-10 06:54:22 +1100

    67. Async await

--- 12-more-concepts/app/router/product.py
+++ 12-more-concepts/app/router/product.py
@@ -2,6 +2,7 @@ from typing import Optional, List
 from fastapi import APIRouter, Header, Cookie, Form
 from fastapi.responses import Response, HTMLResponse, PlainTextResponse
 from app.custom_log import log
+import time
 
 router = APIRouter(
     prefix='/product',
@@ -11,6 +12,11 @@ router = APIRouter(
 products = ['watch', 'camera', 'phone']
 
 
+async def time_consuming_functionality():
+    time.sleep(5)
+    return 'ok'
+
+
 @router.post('/new')
 def create_product(name: str = Form(...)):
     products.append(name)
@@ -18,7 +24,10 @@ def create_product(name: str = Form(...)):
 
 
 @router.get('/all')
-def get_all_products():
+async def get_all_products():
+    # async await example
+    await time_consuming_functionality()
+    # log example
     log("MyAPI", "Call to get all products")
     # return products
     data = " ".join(products)
