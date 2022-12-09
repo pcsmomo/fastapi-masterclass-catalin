088d277669b86dad4c7491714e20f1c308fe616f
Noah Kim <dongwoo_333@hanmail.net>
2022-12-09 16:37:21 +1100

    58. UploadFile

--- 10-files/app/router/file.py
+++ 10-files/app/router/file.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, UploadFile
+import shutil
 
 router = APIRouter(
     prefix='/file',
@@ -11,3 +12,15 @@ def get_file(file: bytes = File(...)):
     content = file.decode('utf-8')
     lines = content.split('\n')
     return {'lines': lines}
+
+
+@router.post('/uploadfile')
+def get_uploadfile(upload_file: UploadFile = File(...)):
+    path = f"app/files/{upload_file.filename}"
+    with open(path, 'w+b') as buffer:
+        shutil.copyfileobj(upload_file.file, buffer)
+
+    return {
+        'filename': path,
+        'type': upload_file.content_type
+    }
