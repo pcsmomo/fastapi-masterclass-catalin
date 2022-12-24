8bf3cd996fe8cbdde09ff9d4c6366ac5e428f3c3
Noah Kim <dongwoo_333@hanmail.net>
2022-12-24 19:12:41 +1100

    111. CORS

--- 17-prj-instagram/api/app/main.py
+++ 17-prj-instagram/api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from app.db import models
 from app.db.database import engine
 from app.routers import user, post, comment
@@ -16,6 +17,21 @@ app.include_router(comment.router)
 @app.get('/')
 def hw():
     return "Hello world!"
+
+
+origins = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 models.Base.metadata.create_all(engine)
