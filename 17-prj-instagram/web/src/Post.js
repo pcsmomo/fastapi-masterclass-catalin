853fbc40b5b7c12d173a784a0a990846bb41271d
Noah Kim <dongwoo_333@hanmail.net>
2022-12-28 08:21:50 +1100

    117. Caption and comments

--- 17-prj-instagram/web/src/Post.js
+++ 17-prj-instagram/web/src/Post.js
@@ -5,6 +5,7 @@ const BASE_URL = 'http://localhost:8000/';
 
 function Post({ post }) {
   const [imageUrl, setImageUrl] = useState('');
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     if (post.image_url_type === 'absolute') {
@@ -14,9 +15,23 @@ function Post({ post }) {
     }
   }, [post.image_url, post.image_url_type]);
 
+  useEffect(() => {
+    setComments(post.comments);
+  }, [post.comments]);
+
   return (
     <div className="post">
       <img className="post_image" src={imageUrl} alt="post" />
+
+      <h4 className="post_text">{post.caption}</h4>
+
+      <div className="post_comments">
+        {comments.map((comment, idx) => (
+          <p key={`${comment.username}-${idx}`}>
+            <strong>{comment.username}:</strong> {comment.text}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
