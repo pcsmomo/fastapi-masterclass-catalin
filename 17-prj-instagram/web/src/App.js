b7292d8549da3d8c81a456c660ee7f4e70ac698d
Noah Kim <dongwoo_333@hanmail.net>
2022-12-28 08:31:29 +1100

    119. Sort posts

--- 17-prj-instagram/web/src/App.js
+++ 17-prj-instagram/web/src/App.js
@@ -18,6 +18,20 @@ function App() {
         throw response;
       })
       .then((data) => {
+        const result = data.sort((a, b) => {
+          const t_a = a.timestamp.split(/[-T:]/);
+          const t_b = b.timestamp.split(/[-T:]/);
+          const d_a = new Date(
+            Date.UTC(t_a[0], t_a[1] - 1, t_a[2], t_a[3], t_a[4], t_a[5])
+          );
+          const d_b = new Date(
+            Date.UTC(t_b[0], t_b[1] - 1, t_b[2], t_b[3], t_b[4], t_b[5])
+          );
+          return d_b - d_a;
+        });
+        return result;
+      })
+      .then((data) => {
         setPosts(data);
       })
       .catch((error) => {
