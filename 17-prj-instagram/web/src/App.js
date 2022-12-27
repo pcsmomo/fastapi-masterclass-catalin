e58e062c8d52589af29af567bd8b41d88fa548e8
Noah Kim <dongwoo_333@hanmail.net>
2022-12-28 08:10:17 +1100

    115. Retrieve posts from backend

--- 17-prj-instagram/web/src/App.js
+++ 17-prj-instagram/web/src/App.js
@@ -1,6 +1,30 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const BASE_URL = 'http://localhost:8000/';
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + 'post/all')
+      .then((response) => {
+        const json = response.json();
+        console.log(json);
+        if (response.ok) {
+          return json;
+        }
+        throw response;
+      })
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }, []);
+
   return 'Hello World';
 }
 
