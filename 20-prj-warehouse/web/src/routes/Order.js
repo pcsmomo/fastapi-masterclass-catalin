2270dff4d47bf2104fde6bd23d69702577e6e5c6
Noah Kim <dongwoo_333@hanmail.net>
2023-01-19 22:52:46 +1100

    182. Create order functionality

--- 20-prj-warehouse/web/src/routes/Order.js
+++ 20-prj-warehouse/web/src/routes/Order.js
@@ -1,9 +1,56 @@
 import { useEffect, useState } from "react";
 
+const PRODUCTS_URL = "http://localhost:8000/";
+const ORDERS_URL = "http://localhost:8001/";
+
 export const Order = () => {
   const [id, setId] = useState("");
   const [quantity, setQuantity] = useState("");
   const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(PRODUCTS_URL + "product/" + id)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        const price = parseFloat(data.price) * 1.2;
+        setMessage(`Your product price is $${price}`);
+      });
+  }, [id]);
+
+  const handleCreate = (event) => {
+    event.preventDefault();
+
+    const json_string = JSON.stringify({
+      product_id: id,
+      quantity: quantity,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: json_string,
+    };
+
+    fetch(ORDERS_URL + "orders", requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw response;
+        }
+      })
+      .then((data) => {
+        setMessage(`Order for ${quantity} items sent`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="body">
@@ -22,7 +69,7 @@ export const Order = () => {
           onChange={(event) => setQuantity(event.target.value)}
         />
       </div>
-      <button className="button-4" onClick={null}>
+      <button className="button-4" onClick={handleCreate}>
         Place order
       </button>
       <div className="form_message">{message}</div>
